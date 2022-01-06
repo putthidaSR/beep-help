b0f57531bfe0015fddcf6263886ea81dd6868d8d
putthidaSR <psamrith@uw.edu>
2022-01-06 15:19:23 -0800

    added email and website link

--- app/src/main/java/com/example/uwcapstone/AboutUsFragment.java
+++ app/src/main/java/com/example/uwcapstone/AboutUsFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +61,16 @@ public class AboutUsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about_us, container, false);
+        View view = inflater.inflate(R.layout.fragment_about_us, container, false);
+
+        // Click to send email
+        final TextView emailLink = (TextView)view.findViewById(R.id.email);
+        emailLink.setMovementMethod(LinkMovementMethod.getInstance());
+
+        // Click to launch browser
+        final TextView websiteLink = (TextView)view.findViewById(R.id.website);
+        websiteLink.setMovementMethod(LinkMovementMethod.getInstance());
+
+        return view;
     }
 }
